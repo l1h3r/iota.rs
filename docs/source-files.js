0ad48f24d661594b1bdd8105ca9b35a46c00b271
Thoralf-M <Thoralf-M@users.noreply.github.com>
2021-03-12 16:21:32 +0000

    deploy: db62b87f01dd89dbc52c19272892426e1e855c24

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["iota"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["iota_client"] = {"name":"","dirs":[{"name":"api","files":["address.rs","balance.rs","message_builder.rs","mod.rs","unspent.rs"]},{"name":"node","files":["address.rs","message.rs","mod.rs"]},{"name":"storage","files":["mod.rs","sqlite.rs"]}],"files":["builder.rs","client.rs","error.rs","lib.rs"]};
+sourcesIndex["iota_client"] = {"name":"","dirs":[{"name":"api","files":["address.rs","balance.rs","message_builder.rs","mod.rs","unspent.rs"]},{"name":"node","files":["address.rs","message.rs","mod.rs","mqtt.rs"]},{"name":"storage","files":["mod.rs","sqlite.rs"]}],"files":["builder.rs","client.rs","error.rs","lib.rs"]};
 createSourceSidebar();
