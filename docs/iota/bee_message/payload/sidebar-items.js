cbbc77ecae1febbc5a404a63110373c6355a2eee
melatron <melatron@users.noreply.github.com>
2021-03-23 07:15:17 +0000

    deploy: f4a764989e065955f7d261701090705619b73e13

--- docs/iota/bee_message/payload/sidebar-items.js
+++ docs/iota/bee_message/payload/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Payload",""]],"fn":[["pack_option_payload",""],["unpack_option_payload",""]],"mod":[["indexation",""],["milestone",""],["receipt",""],["transaction",""],["treasury",""]]});
+initSidebarItems({"enum":[["Payload",""]],"fn":[["option_payload_pack",""],["option_payload_packed_len",""],["option_payload_unpack",""]],"mod":[["indexation",""],["milestone",""],["receipt",""],["transaction",""],["treasury",""]]});
